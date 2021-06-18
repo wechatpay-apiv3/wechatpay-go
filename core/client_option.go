9a63b77b609497b99eee91ac2742acbff1951084
EmmetZC <emmetzc@gmail.com>
2021-06-18 10:32:30 +0800

    doc: 增加导出类型的文档，消除代码规范错误

--- core/client_option.go
+++ core/client_option.go
@@ -21,31 +21,40 @@ func (w ErrorOption) Apply(o *DialSettings) error {
 	return w.Error
 }
 
+// WithSignerOption 为 Client 设置 Signer
 type WithSignerOption struct {
 	Signer auth.Signer
 }
 
+// Apply 将配置添加到 DialSettings 中
 func (w WithSignerOption) Apply(o *DialSettings) error {
 	o.Signer = w.Signer
 	return nil
 }
 
-type WithValidatorOption struct{ Validator auth.Validator }
+// WithValidatorOption 为 Client 设置 Validator
+type WithValidatorOption struct {
+	Validator auth.Validator
+}
 
-// Apply 将配置添加到DialSettings中
+// Apply 将配置添加到 DialSettings 中
 func (w WithValidatorOption) Apply(o *DialSettings) error {
 	o.Validator = w.Validator
 	return nil
 }
 
-type WithHTTPClientOption struct{ Client *http.Client }
+// WithHTTPClientOption 为 Client 设置 HTTPClient
+type WithHTTPClientOption struct {
+	Client *http.Client
+}
 
-// Apply 将配置添加到DialSettings中
+// Apply 将配置添加到 DialSettings 中
 func (w WithHTTPClientOption) Apply(o *DialSettings) error {
 	o.HTTPClient = w.Client
 	return nil
 }
 
+// WithTimeoutOption 为 Client 设置 超时时间
 type WithTimeoutOption time.Duration
 
 // Apply 将配置添加到DialSettings中
@@ -54,17 +63,23 @@ func (w WithTimeoutOption) Apply(o *DialSettings) error {
 	return nil
 }
 
-type WithHeaderOption struct{ Header http.Header }
+// WithHeaderOption 为 Client 设置 DefaultHeader
+type WithHeaderOption struct {
+	Header http.Header
+}
 
-// Apply 将配置添加到DialSettings中
+// Apply 将配置添加到 DialSettings 中
 func (w WithHeaderOption) Apply(o *DialSettings) error {
 	o.Header = w.Header
 	return nil
 }
 
-type WithCipherOption struct{ Cipher cipher.Cipher }
+// WithCipherOption 为 Client 设置 Cipher
+type WithCipherOption struct {
+	Cipher cipher.Cipher
+}
 
-// Apply 将配置添加到DialSettings中
+// Apply 将配置添加到 DialSettings 中
 func (w WithCipherOption) Apply(o *DialSettings) error {
 	o.Cipher = w.Cipher
 	return nil
