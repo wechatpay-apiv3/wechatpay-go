e9ade2bf1ca143ff16d396de4fe9aa04edf9d1e9
EmmetZC <emmetzc@gmail.com>
2021-07-01 17:23:01 +0800

    test: test `notify.go` without `package consts`

--- core/notify/notify_test.go
+++ core/notify/notify_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
 	"github.com/wechatpay-apiv3/wechatpay-go/core/auth/verifiers"
-	"github.com/wechatpay-apiv3/wechatpay-go/core/consts"
 	"github.com/wechatpay-apiv3/wechatpay-go/utils"
 
 	"github.com/agiledragon/gomonkey"
@@ -43,24 +42,14 @@ func Test_getRequestBody(t *testing.T) {
 }
 
 type contentType struct {
-	ContractId      *string    `json:"contract_id"`
 	Mchid           *string    `json:"mchid"`
 	Appid           *string    `json:"appid"`
-	Openid          *string    `json:"openid"`
-	PlanId          *string    `json:"plan_id"`
-	ContractStatus  *string    `json:"contract_status"`
 	CreateTime      *time.Time `json:"create_time"`
 	OutContractCode *string    `json:"out_contract_code"`
 }
 
 func (o contentType) String() string {
 	ret := ""
-
-	if o.ContractId == nil {
-		ret += "ContractId:<nil>,"
-	} else {
-		ret += fmt.Sprintf("ContractId:%v,", *o.ContractId)
-	}
 
 	if o.Mchid == nil {
 		ret += "Mchid:<nil>,"
@@ -72,24 +61,6 @@ func (o contentType) String() string {
 		ret += "Appid:<nil>,"
 	} else {
 		ret += fmt.Sprintf("Appid:%v,", *o.Appid)
-	}
-
-	if o.Openid == nil {
-		ret += "Openid:<nil>,"
-	} else {
-		ret += fmt.Sprintf("Openid:%v,", *o.Openid)
-	}
-
-	if o.PlanId == nil {
-		ret += "PlanId:<nil>,"
-	} else {
-		ret += fmt.Sprintf("PlanId:%v,", *o.PlanId)
-	}
-
-	if o.ContractStatus == nil {
-		ret += "ContractStatus:<nil>,"
-	} else {
-		ret += fmt.Sprintf("ContractStatus:%v,", *o.ContractStatus)
 	}
 
 	if o.CreateTime == nil {
@@ -108,20 +79,52 @@ func (o contentType) String() string {
 }
 
 func TestHandler_ParseNotifyRequest(t *testing.T) {
-	patch := gomonkey.ApplyFunc(time.Now, func() time.Time {
-		return time.Unix(1624523846, 0)
-	})
+	patch := gomonkey.ApplyFunc(
+		time.Now, func() time.Time {
+			return time.Unix(1624523846, 0)
+		},
+	)
 	defer patch.Reset()
+
+	const (
+		mchAPIv3Key          = "testMchAPIv3Key0"
+		wechatPayCertificate = `-----BEGIN CERTIFICATE-----
+MIIDVzCCAj+gAwIBAgIJANfOWdH1ItcBMA0GCSqGSIb3DQEBCwUAMEIxCzAJBgNV
+BAYTAlhYMRUwEwYDVQQHDAxEZWZhdWx0IENpdHkxHDAaBgNVBAoME0RlZmF1bHQg
+Q29tcGFueSBMdGQwHhcNMjEwNDI3MDg1NTIzWhcNMzEwNDI1MDg1NTIzWjBCMQsw
+CQYDVQQGEwJYWDEVMBMGA1UEBwwMRGVmYXVsdCBDaXR5MRwwGgYDVQQKDBNEZWZh
+dWx0IENvbXBhbnkgTHRkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+2VCTd91fnUn73Xy9DLvt/V62TVxRTEEstVdeRaZ3B3leO0pldE806mXO4RwdHXag
+HQ4vGeZN0yqm++rDsGK+U3AH7kejyD2pXshNP9Cq5YwbptiLGtjcquw4HNxJQUOm
+DeJf2vg6byms9RUipiq4SzbJKqJFlUpbuIPDpSpWz10PYmyCNeDGUUK65E5h2B83
+4uxl1zNLYQCrkdBzb8oUxwYeP5a2DNxmjL5lsJML7DGr5znsevnoqGRwTm9fxCGf
+y8wus7hwKz6clt3Whmmda7UAdb1c08hEQFVRbF14AR73xbnd8N0obCWJPCbzMCtk
+aSef4FdEEgEXJiw0VAJT8wIDAQABo1AwTjAdBgNVHQ4EFgQUT1c7nd/SUO76HSoZ
+umNUJv1R5PwwHwYDVR0jBBgwFoAUT1c7nd/SUO76HSoZumNUJv1R5PwwDAYDVR0T
+BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAfTjxKRQMzNB/U6ZoCUS+BSNfa2Oh
+0plMN6ZuzwiVVZwg1jywvv5yv04koS7Pd4i9E4gt9ZBUQXlpq+A3oOCEEHNRR6b2
+kyazGRM7s0OP5X21WrbpSmKmU6K7hkfx30yYs08LVs/Q8DIhvaj1FCFeJzUCzYn/
+fHMq4tsbKO0dKAeydPM/nrUZBmaYQVKMVOORGLFjFKVO7JV6Kq/R86ouhjEPgJOe
+2xulNBUcjicqtZlBdEh/PWCYP2SpGVDclKm8jeo175T3EVAkdKzzmfpxtMmnMlmq
+cTJOU9TxuGvNASMtjj7pYIerTx+xgZDXEVBWFW9PjJ0TV06tCRsgSHItgg==
+-----END CERTIFICATE-----`
+		data = "{" +
+			"\"mchid\":\"1234567890\"," +
+			"\"appid\":\"054aa7d7a2a54ab5898df65bd96f001c\"," +
+			"\"create_time\":\"2020-06-30T12:12:00+08:00\"," +
+			"\"out_contract_code\":\"21640bdbd08e473e828f3206a2741c6e\"" +
+			"}"
+	)
 
 	headers := map[string]string{
 		"Content-Type":        "application/json",
 		"Request-Id":          "0885F2CF8606108F0518E29E944820F10B28E24A",
 		"Wechatpay-Nonce":     "EcZ9Cmy4Xyx1i6RlJQzLcCyEqDa26NBz",
 		"Wechatpay-Timestamp": "1624523846",
-		"Wechatpay-Serial":    "714E57301CE0FD6267DA19B19E08CEE431E6D943",
-		"Wechatpay-Signature": "E/RMTykUevOG5ZMjk7z+pm/EpVGM9VbmTlhVm+7R4cu/wd6Dtk2jKgzgBSAwL6wL8484lz9V5Xi8n2FO6oDAstwy/YOBpHdB7PH/Za/WUN8Op2CGpxdZNepn+PLWor/TKhSm9cb0owg1m2yNvEdHmtrDe3hD8IHJAWRovdBybc+41YLJuFSh5gRD7OtLyYsj3w7Zrac+CARdr8iF0QTvLhTv7rN45Tv59g/n9TpNxSnvj4nHb74L6EhjCwN17rnq9IR7wxfnOtJHVbDaCOm5xy4pWA+d3uvJrF/SfE0FmBhn1KQTlKfveGAiatSXl6qrIwWmjaiqcsTZy7ZEP1M/Wg==",
+		"Wechatpay-Serial":    "D7CE59D1F522D701",
+		"Wechatpay-Signature": "tJHIiIS9eB2hAYstmAmbbD3ZE5LiIm/Ug5tuL4fC0YOFRWIHV39UFIZXC0e9Wl6lBu6sKvkqDkzpqzBsVHyXFlbYZTOQrVdG4b6LfTnK4mikv9++ixJMd3vTf2yCqvBkh98zs3Ds5zsYQakzbcwhmw4fMJs4nPLws28H0UW9FjDR//rxELLwXvV1VEA1IBLX70xptjL8hrfUjEE8kkry6yNJTHZRU8CAc7qHli2Ng1V1qb9ARbK8A3ThmFmPQvRGrapI/jS2laKKgYUmfdEdkNO6B2Cke5e8VTxY406ArAmQ90GAihDwIcb16TQMnzCMBoutnwZKNiKRACrFmtxw2Q==",
 	}
-	body := `{"id":"3119dfba-e649-5eec-ab1e-3412bc4d2e17","create_time":"2021-06-24T16:37:26+08:00","resource_type":"encrypt-resource","event_type":"PAYSCORE.USER_OPEN_SERVICE","summary":"签约成功","resource":{"original_type":"payscore","algorithm":"AEAD_AES_256_GCM","ciphertext":"857DUp4FqJhj2AxF63p333erLhhcG2glNoAKeP3ArRC3RbZyL4INrdmqzduerALpJAJ7mF0rYN0gbQBEYc89N3wvExHJsnre80AhBQydIF5IhX5xuzjlw682dC5aP317H2UyDOCobQHSJsTyIoty/2AY30q/Cc7yz4TBVgNAgMMLFWAsrq+T/oguNQxmxKglPgxTEXM5N42UdLPYwOmy0EZZqOT5Lt5ppwnSJrJPX7qGha5Ba27VmOgNf5u15WVlEzgY1ryba4ijLmQCix70Pgnq45emUT8iGMrXRpJcM9vnMxFjgabI9prlH0lmruEZd0o4FFMbRIESGfPYvRyq7qDMp+WiRXnUOkso8Dw4F9GBNTK1XgukPPgPLblHro2O9Z+7HUfizh/9KDa/7JwdhJX58r5NmV4wZEyootmjOFA4jNetfEvUccXLpDO82/pUZ5IhSw==","associated_data":"payscore","nonce":"Kj7QIyUiYx1q"}}`
+	body := `{"id":"3119dfba-e649-5eec-ab1e-3412bc4d2e17","create_time":"2021-06-24T16:37:26+08:00","resource_type":"encrypt-resource","event_type":"PAYSCORE.USER_OPEN_SERVICE","summary":"签约成功","resource":{"original_type":"payscore","algorithm":"AEAD_AES_256_GCM","ciphertext":"YDS3lKPaC4Y52Gf3uhft5qUBlIa8b428AWTtTauHQfQrRw+X1WpiuHIDy0vo1Vd6VEq67aVyqPdDYMkRVSDaZL3iZttevRMOoPKMifozg6XPWjIZumks/GpT48lI4NizyeaqLBokNebthah3o1H76qSlO9NkDjp9bzmKLEYYH9TEklFUpsvPqOTOcgSLgh21YJXYR7dEBXFgRLiNIKRgO5JdXh1hccRUAlyVWxE54PXpnQ==","associated_data":"payscore","nonce":"Kj7QIyUiYx1q"}}`
 
 	bodyBuf := &bytes.Buffer{}
 	bodyBuf.WriteString(body)
@@ -133,15 +136,18 @@ func TestHandler_ParseNotifyRequest(t *testing.T) {
 		req.Header.Set(key, value)
 	}
 
-	cert, err := utils.LoadCertificate(consts.WechatPayCertificate)
+	cert, err := utils.LoadCertificate(wechatPayCertificate)
 	require.NoError(t, err)
 
-	handler := NewNotifyHandler(consts.MchAPIv3Key, verifiers.NewSHA256WithRSAVerifier(core.NewCertificateMapWithList([]*x509.Certificate{cert})))
+	handler := NewNotifyHandler(
+		mchAPIv3Key, verifiers.NewSHA256WithRSAVerifier(core.NewCertificateMapWithList([]*x509.Certificate{cert})),
+	)
 
 	content := new(contentType)
 
 	notifyReq, err := handler.ParseNotifyRequest(context.Background(), req, content)
 	require.NoError(t, err)
+
 	assert.Equal(t, "3119dfba-e649-5eec-ab1e-3412bc4d2e17", notifyReq.ID)
 	assert.Equal(t, "2021-06-24T16:37:26+08:00", notifyReq.CreateTime.Format(time.RFC3339))
 	assert.Equal(t, "encrypt-resource", notifyReq.ResourceType)
@@ -152,6 +158,11 @@ func TestHandler_ParseNotifyRequest(t *testing.T) {
 	assert.Equal(t, "AEAD_AES_256_GCM", notifyReq.Resource.Algorithm)
 	assert.Equal(t, "payscore", notifyReq.Resource.OriginalType)
 
-	t.Log(content)
-	t.Log(notifyReq.Resource.Plaintext)
+	assert.Equal(t, data, notifyReq.Resource.Plaintext)
+
+	assert.Equal(t, "1234567890", *content.Mchid)
+	assert.Equal(t, "054aa7d7a2a54ab5898df65bd96f001c", *content.Appid)
+	assert.Equal(t, "21640bdbd08e473e828f3206a2741c6e", *content.OutContractCode)
+	createTime, _ := time.Parse(time.RFC3339, "2020-06-30T12:12:00+08:00")
+	assert.Zero(t, content.CreateTime.Sub(createTime))
 }
