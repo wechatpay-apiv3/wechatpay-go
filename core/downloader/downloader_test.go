7f6ae3ac9784e74661edfff08041830140964a0d
EmmetZC <emmetzc@gmail.com>
2021-07-02 11:49:13 +0800

    test: tests of package downloader

--- core/downloader/downloader_test.go
+++ core/downloader/downloader_test.go
@@ -7,42 +7,55 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
-	"github.com/wechatpay-apiv3/wechatpay-go/core/consts"
 	"github.com/wechatpay-apiv3/wechatpay-go/core/downloader"
 	"github.com/wechatpay-apiv3/wechatpay-go/core/option"
 	"github.com/wechatpay-apiv3/wechatpay-go/utils"
 )
 
 func TestNewCertificateDownloaderWithClient(t *testing.T) {
+	patches := mockDownloadServer(t)
+	defer patches.Reset()
+
 	ctx := context.Background()
 
-	privateKey, err := utils.LoadPrivateKey(consts.MchPrivateKey)
+	privateKey, err := utils.LoadPrivateKey(mockMchPrivateKey)
 	require.NoError(t, err)
 	opts := []core.ClientOption{
-		option.WithMerchantCredential(consts.MchID, consts.SerialNo, privateKey),
+		option.WithMerchantCredential(mockMchID, mockMchCertificateSerial, privateKey),
 		option.WithoutValidator(),
 	}
 
 	client, err := core.NewClient(ctx, opts...)
 	require.NoError(t, err)
 
-	d, err := downloader.NewCertificateDownloaderWithClient(ctx, client, consts.MchAPIv3Key)
+	d, err := downloader.NewCertificateDownloaderWithClient(ctx, client, mockAPIv3Key)
 	require.NoError(t, err)
 
 	assert.NotEmpty(t, d.GetAll(ctx))
 	for serialNo, cert := range d.GetAll(ctx) {
 		assert.Equal(t, serialNo, utils.GetCertificateSerialNumber(*cert))
 	}
+
+	// call Download
+	err = d.DownloadCertificates(ctx)
+	require.NoError(t, err)
+
+	// call Download Again
+	err = d.DownloadCertificates(ctx)
+	require.NoError(t, err)
 }
 
 func TestNewCertificateDownloader(t *testing.T) {
-	privateKey, err := utils.LoadPrivateKey(consts.MchPrivateKey)
+	patches := mockDownloadServer(t)
+	defer patches.Reset()
+
+	privateKey, err := utils.LoadPrivateKey(mockMchPrivateKey)
 	require.NoError(t, err)
 
 	ctx := context.Background()
 
 	d, err := downloader.NewCertificateDownloader(
-		context.Background(), consts.MchID, privateKey, consts.SerialNo, consts.MchAPIv3Key,
+		context.Background(), mockMchID, privateKey, mockMchCertificateSerial, mockAPIv3Key,
 	)
 	require.NoError(t, err)
 
@@ -50,4 +63,12 @@ func TestNewCertificateDownloader(t *testing.T) {
 	for serialNo, cert := range d.GetAll(ctx) {
 		assert.Equal(t, serialNo, utils.GetCertificateSerialNumber(*cert))
 	}
+
+	// call Download
+	err = d.DownloadCertificates(ctx)
+	require.NoError(t, err)
+
+	// call Download Again
+	err = d.DownloadCertificates(ctx)
+	require.NoError(t, err)
 }
